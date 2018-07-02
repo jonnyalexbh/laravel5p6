815cf21088ec8a2175f00847e8b2906ed54339fc
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-07-02 18:53:06 -0500

    user one-to-one relationship with gender

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
   protected $hidden = [
     'password', 'remember_token',
   ];
+  /**
+  * gender One To One
+  *
+  */
+  public function gender()
+  {
+    return $this->hasOne('App\Gender', 'id', 'gender_id');
+  }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <div class="card">
+          <div class="card-header">Users</div>
+
+          <div class="card-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <td>ID</td>
+                  <td>Name</td>
+                  <td>Birth</td>
+                  <td>Gender</td>
+                  <td>Email</td>
+                  <td>Phone</td>
+                </tr>
+              </thead>
+              @foreach ($users as $user)
+                <tbody>
+                  <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->dt_birth }}</td>
+                    <td>{{ $user->gender->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->phone }}</td>
+                  </tr>
+                </tbody>
+              @endforeach
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
