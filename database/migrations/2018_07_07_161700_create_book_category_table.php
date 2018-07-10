ae001f85a1b8c5e433d30267f93b6676a855e873
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-07-10 17:45:39 -0500

    primary book_category

--- database/migrations/2018_07_07_161700_create_book_category_table.php
+++ database/migrations/2018_07_07_161700_create_book_category_table.php
@@ -16,6 +16,7 @@ class CreateBookCategoryTable extends Migration
     Schema::create('book_category', function (Blueprint $table) {
       $table->unsignedInteger('book_id');
       $table->unsignedInteger('category_id');
+      $table->primary(['book_id', 'category_id']);
 
       $table->foreign('book_id')->references('id')->on('books');
       $table->foreign('category_id')->references('id')->on('categories');
