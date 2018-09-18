9009490f17636de93e66f43a0ff66b265318290a
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-09-18 16:06:12 -0500

    frontend quick start with passport

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,13 @@
                 {{ session('status') }}
               </div>
             @endif
-            
+
             You are logged in!
+
+            <passport-clients></passport-clients>
+            <passport-authorized-clients></passport-authorized-clients>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
+
           </div>
         </div>
       </div>
