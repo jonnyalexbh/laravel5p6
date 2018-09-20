6bdc206740d2a274aa0e5297ad8dde19d63ea9e5
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-09-20 15:42:53 -0500

    verify if email exists login github

--- app/Http/Controllers/Auth/SocialProviderController.php
+++ app/Http/Controllers/Auth/SocialProviderController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
+use App\User;
 use Socialite;
+use App\SocialProvider;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -22,7 +25,41 @@ class SocialProviderController extends Controller
   */
   public function handleProviderCallback($provider)
   {
-    $user = Socialite::driver($provider)->user();
-    dd($user);
+    try {
+      $socialiteUser = Socialite::driver($provider)->user();
+    } catch (Exception $e) {
+      return redirect('/');
+    }
+    // check if that user already exists in the DB
+    $findUser = SocialProvider::where('provider_id', $socialiteUser->getId())->first();
+
+    if ($findUser) {
+      $user = $findUser->user;
+    }
+    // otherwise create a new user
+    else {
+      $user = User::firstOrCreate([
+        'email' => $socialiteUser->getEmail()
+      ], [
+        'name' => $socialiteUser->getName(),
+        'password' => bcrypt('secret'),
+        'gender_id' => \App\Gender::all()->random()->id,
+        'phone' => 2222222,
+      ]);
+      $user->socialProviders()->create([
+        'provider_id' => $socialiteUser->getId(),
+        'provider' => $provider
+      ]);
+    }
+    return $this->authAndRedirect($user);
+  }
+  /**
+  * login and redirection
+  *
+  */
+  public function authAndRedirect($user)
+  {
+    Auth::login($user);
+    return redirect('home');
   }
 }
--- app/User.php
+++ app/User.php
@@ -47,6 +47,11 @@ class User extends Authenticatable
   {
     return $this->belongsTo('App\MaritalStatus');
   }
+
+  public function socialProviders()
+  {
+    return $this->hasMany('App\SocialProvider');
+  }
   /**
   * accessors
   *
